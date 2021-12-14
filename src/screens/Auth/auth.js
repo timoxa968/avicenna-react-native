6b29d67eb824c2adfab719debf2dc852e90a7668
Timoxa <timofey.b94@gmail.com>
2021-12-14 17:54:24 +0600

    added aitu auth

--- src/screens/Auth/auth.js
+++ src/screens/Auth/auth.js
@@ -1,27 +1,63 @@
 import axios from 'axios';
-import * as React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Image, Modal, StyleSheet} from 'react-native';
+import Logo from '../../assets/Images/Auth-logo.png';
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
-import aituBridge from '@btsd/aitu-bridge';
+import {WebView} from 'react-native-webview';
+// import aituBridge from '@btsd/aitu-bridge';
 
-async function getData() {
-  try {
-    const result = aituBridge.supports('openSettings');
-    console.log(result);
-  } catch (err) {
-    // Handle Error Here
-    console.error(err);
-  }
-}
+// async function getData() {
+//   try {
+//     // invoke method
+//     const data = await aituBridge.getMe();
+//     // handle data
+//   } catch (e) {
+//     // handle error
+//     console.log(e);
+//   }
+// }
 
 function Auth() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const auth =
+    'https://passport.aitu.io/oauth2/auth?response_type=code&client_id=453c31f3-15ed-45e2-a8de-5f2d832112bd&scope=openid%20phone%20id_card_manual&redirect_uri=https://test.avicenna.online/login.xhtml&state=12345678';
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Auth Screen</Text>
+      <Image style={styles.logo} source={Logo} />
       <Input placeholder={'Введите ИИН'} />
-      <Button value={'Текст кнопки'} onPress={getData} />
+      <Button
+        value={'Войти через AITU'}
+        onPress={() => setModalVisible(true)}
+      />
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={{flex: 1}}>
+          <WebView
+            onNavigationStateChange={WebViewState =>
+              this._onNavigationStateChange(WebViewState)
+            }
+            source={{
+              uri: auth,
+            }}
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    maxWidth: 350,
+    maxHeight: 320,
+  },
+});
+
 export default Auth;
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,15 +1,28 @@
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text} from 'react-native';
+import Button from '../../components/Button/Button';
 
-function HomeScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home asd123Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Auth')}
-      />
-    </View>
-  );
+class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+      loading: false,
+    };
+  }
+  render() {
+    const {navigation} = this.props;
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Главная страница</Text>
+        <Button
+          style={{margin: 50}}
+          value="Авторизация"
+          onPress={() => navigation.navigate('Auth')}
+        />
+      </View>
+    );
+  }
 }
+
 export default HomeScreen;
